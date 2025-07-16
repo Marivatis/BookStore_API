1b84ce5107e745738cb9eb89e36fcfbf237b52a6
Marivatis <martyniuk.tech@gmail.com>
2025-07-16 15:02:11 +0300

    feat(entity): add Order entity and OrderItem struct

---
+++ internal/entity/order.go
@@ -0,0 +1,15 @@
+package entity
+
+import "time"
+
+type Order struct {
+	Id        int
+	Items     []OrderItem
+	Status    string
+	CreatedAt time.Time
+}
+
+type OrderItem struct {
+	Product  Product
+	Quantity int
+}
