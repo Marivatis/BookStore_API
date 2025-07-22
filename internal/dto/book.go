37bf96dc53cead0dc8b4b4d81d86ba74b6b91866
Marivatis <martyniuk.tech@gmail.com>
2025-07-22 16:26:50 +0300

    feat(dto): add book DTOs and conversion methods between DTO and entity

---
+++ internal/dto/book.go
@@ -0,0 +1,68 @@
+package dto
+
+import (
+	"BookStore_API/internal/entity"
+	"time"
+)
+
+type BookCreateRequest struct {
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+	Author string  `json:"author"`
+	Isbn   string  `json:"isbn"`
+}
+
+type BookUpdateRequest struct {
+	Name   *string  `json:"name"`
+	Price  *float64 `json:"price"`
+	Author *string  `json:"author"`
+	Isbn   *string  `json:"isbn"`
+}
+
+type BookResponse struct {
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Price     float64   `json:"price"`
+	Author    string    `json:"author"`
+	Isbn      string    `json:"isbn"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func FromEntityBook(b entity.Book) BookResponse {
+	return BookResponse{
+		Id:        b.Id,
+		Name:      b.Name,
+		Price:     b.Price,
+		Author:    b.Author,
+		Isbn:      b.Isbn,
+		CreatedAt: b.CreatedAt,
+	}
+}
+
+// ToEntity method has pointer receiver in case future logic mutates the receiver.
+func (r *BookCreateRequest) ToEntity() entity.Book {
+	return entity.Book{
+		BaseProduct: entity.BaseProduct{
+			Name:  r.Name,
+			Price: r.Price,
+		},
+		Author: r.Author,
+		Isbn:   r.Isbn,
+	}
+}
+
+// ApplyToEntity method has pointer receiver in case future logic mutates the receiver.
+func (r *BookUpdateRequest) ApplyToEntity(b *entity.Book) {
+	if r.Name != nil {
+		b.Name = *r.Name
+	}
+	if r.Price != nil {
+		b.Price = *r.Price
+	}
+	if r.Author != nil {
+		b.Author = *r.Author
+	}
+	if r.Isbn != nil {
+		b.Isbn = *r.Isbn
+	}
+}
