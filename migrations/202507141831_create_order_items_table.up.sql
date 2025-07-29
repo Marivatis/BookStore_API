f6e5c9d7c1cfbc6627d83fe96e532678a67f96f2
Marivatis <martyniuk.tech@gmail.com>
2025-07-29 14:02:07 +0300

    fix(migrations): change order_items FK reference from books(product_id) to products(id)

--- migrations/202507141831_create_order_items_table.up.sql
+++ migrations/202507141831_create_order_items_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE order_items (
     order_id SERIAL NOT NULL,
-    book_id SERIAL NOT NULL,
+    product_id SERIAL NOT NULL,
     quantity INT NOT NULL DEFAULT 1,
     price NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (order_id, book_id),
+    PRIMARY KEY (order_id, product_id),
     CONSTRAINT fk_order
          FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
     CONSTRAINT fk_books
-         FOREIGN KEY (book_id) REFERENCES books(product_id)
+         FOREIGN KEY (product_id) REFERENCES products(id)
 );
