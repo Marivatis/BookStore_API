b44e9e833eb8e527b9a94b3f001cdfdc9d400a18
Marivatis <martyniuk.tech@gmail.com>
2025-07-29 16:06:15 +0300

    feat(entity): add new methods to Product interface

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -4,9 +4,13 @@ import "time"
 
 type Product interface {
 	GetId() int
+	SetId(id int)
 	GetName() string
+	SetName(name string)
 	GetPrice() float64
+	SetPrice(price float64)
 	GetStock() int
+	SetStock(stock int)
 }
 
 type BaseProduct struct {
@@ -20,15 +24,27 @@ type BaseProduct struct {
 func (b *BaseProduct) GetId() int {
 	return b.Id
 }
+func (b *BaseProduct) SetId(id int) {
+	b.Id = id
+}
 
 func (b *BaseProduct) GetName() string {
 	return b.Name
+}
+func (b *BaseProduct) SetName(name string) {
+	b.Name = name
 }
 
 func (b *BaseProduct) GetPrice() float64 {
 	return b.Price
 }
+func (b *BaseProduct) SetPrice(price float64) {
+	b.Price = price
+}
 
 func (b *BaseProduct) GetStock() int {
 	return b.Stock
+}
+func (b *BaseProduct) SetStock(stock int) {
+	b.Stock = stock
 }
