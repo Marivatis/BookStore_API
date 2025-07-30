7207ae2c34d6135627ed6dcab0f3d86ab3449fb0
Marivatis <martyniuk.tech@gmail.com>
2025-07-30 18:22:01 +0300

    feat(entity): add order.Status constants

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -2,6 +2,16 @@ package entity
 
 import "time"
 
+const (
+	OrderStatusCreated   = "created"
+	OrderStatusAccepted  = "accepted"
+	OrderStatusPending   = "pending"
+	OrderStatusPaid      = "paid"
+	OrderStatusShipped   = "shipped"
+	OrderStatusDelivered = "delivered"
+	OrderStatusCanceled  = "canceled"
+)
+
 type Order struct {
 	Id        int
 	Items     []OrderItem
